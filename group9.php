ad3fa30896ae979b939e603c1ef925ed4b4db829
GodChris <1358643081@outlook.com>
2016-11-24 17:01:11 +0800

    Signed-off-by: GodChris <1358643081@outlook.com>

---
+++ group9.php
@@ -0,0 +1,97 @@
+﻿<?php
+
+$con=mysqli_connect("localhost","root","guanmin1031","studb");
+mysqli_set_charset($con,"utf8");
+$sql="select * from members";
+$query=mysqli_query($con,$sql);
+if($query&&mysqli_fetch_assoc($query))
+{while($row=mysqli_fetch_assoc($query))
+{$data[]=$row;}}
+else
+{$data=array();}
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="all" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>group-9</title>
+    <link rel="stylesheet" href="css/more.css" />
+    <meta http-equiv="refresh" content="10"/>
+
+
+
+</head>
+<body background="img/bg.jpg">
+<div class="header">
+    <div class="title1"><p>南昌大学物联网实验基地软件组第九组成员信息</p></div>
+</div>
+<hr>
+<div class="container">
+
+    <div class="row">
+        <div id="enlarge_images"></div>
+        <div class="col-md-3">
+            <div id="demo"><img src="img/kb.jpg" alt="" /></div>
+            <script type="text/javascript" src="js/g9.js"></script>
+        </div>
+        <div class="col-md-3 yangshi">
+            <ul>
+                <li>姓名：<?php echo $data[0]['name'];?></li>
+                <li>性别：<?php echo $data[0]['gender'];?></li>
+                <li>班级：<?php echo $data[0]['class'];?></li>
+                <li>电话：<?php echo $data[0]['tel'];?></li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <div><img src="img/rw.png" alt="" /></div>
+        </div>
+        <div class="col-md-3 yangshi">
+
+            <ul>
+                <li>姓名：<?php echo $data[1]['name'];?></li>
+                <li>性别：<?php echo $data[1]['gender'];?></li>
+                <li>班级：<?php echo $data[1]['class'];?></li>
+                <li>电话：<?php echo $data[1]['tel'];?></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<hr />
+
+
+
+<div class="container" >
+    <div class="row">
+        <div class="col-md-3">
+        <div><img src="img/sc.jpeg" alt="img/.5.jpg" /></div>
+        </div>
+        <div class="col-md-3 yangshi">
+            <ul>
+                <li>姓名：<?php echo $data[2]['name'];?></li>
+                <li>性别：<?php echo $data[2]['gender'];?></li>
+                <li>班级：<?php echo $data[2]['class'];?></li>
+                <li>电话：<?php echo $data[2]['tel'];?></li>
+            </ul>
+        </div>
+
+
+        <div class="col-md-3">
+        <div><img src="img/ki.jpg" alt="" /></div>
+        </div>
+        <div class="col-md-3 yangshi">
+            <ul>
+                <li>姓名：<?php echo $data[0]['name'];?></li>
+                <li>性别：<?php echo $data[0]['gender'];?></li>
+                <li>班级：<?php echo $data[0]['class'];?></li>
+                <li>电话：<?php echo $data[0]['tel'];?></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+</body>
+	
---
+++ ncu.php
@@ -0,0 +1,205 @@
+﻿
+<!doctype html>
+<html>
+	<head>
+	
+		<title>软件组</title>
+		<link rel="stylesheet" href="css/ncu.css" />
+		<script type="text/javascript" src="js/ncu.js"></script>
+		<script type="text/javascript" language="javascript">
+			window.onload=function(){
+
+				ncu.scroll(4,"banner_list","list","banner_info");
+			}
+		</script>
+		<meta http-equiv="refresh" content="10"/>
+	</head>
+	<body>
+
+	<!--南昌大学logo  -->
+	<div class="toubu"><a href="#"><img class="logo"src="img/ncu.png" alt="南昌大学logo" /></a>
+		<!-- 右边邮箱一行 -->
+		<div class="headright">
+			<ul class="oa">
+				<li><img src="img/iconfont-user.png" alt="" class="mail"/><a href="">邮箱</a></li>
+				<li><img src="img/iconfont-iconallfile.png" alt="" class="mail" /><a href="">OA系统</a></li>
+				<li><img src="img/iconfont-edit.png" alt="" class="mail"/><a href="">language</a></li>
+			</ul>
+			<ul class="oa1">
+				<li class="a1"> <a href="http://www.ncu.edu.cn/ks/">导师</a></li>
+				<li class="a2"> <a href="http://www.ncu.edu.cn/zxs/">成员</a></li>
+				<li class="a3"> <a href="http://www.ncu.edu.cn/jzg/">项目组</a></li>
+				<li class="a4"> <a href="http://www.ncu.edu.cn/xy/">合伙人</a></li>
+				<li class="a5"> <a href="http://www.ncu.edu.cn/fk/">访客</a></li>
+			</ul>
+
+		</div>
+		</div>
+
+		<div class="daohang">
+			<ul class="menu">
+				<li><a href="#">首页</a></li>
+				<li><a href="#">基地概况</a></li>
+				<li><a href="#">学院与学科</a></li>
+				<li><a href="#">职能部门</a></li>
+				<li><a href="#">人才培养</a></li>
+				<li><a href="#">科学研究</a></li>
+				<li><a href="#">招生</a></li>
+				<li><a href="#">管理与服务</a></li>
+			</ul>
+		
+		<div class="search">
+			<input type="" class="searchinput" placeholder="搜索..."/>
+			<button type=“submit” class="searchBth">搜索</button>
+		</div>
+	</div>
+	<div id="banner">
+		<div id="banner_bg"></div>
+		<a href="#" id="banner_info"></a>
+		<ul id="list"></ul>
+		<div id="banner_list">
+			<a href="#" target="_blank"><img src="img/1.png" alt="" /></a>
+			<a href="#" target="_blank"><img src="img/1b.jpg" alt="" /></a>
+			<a href="#" target="_blank"><img src="img/1c.jpg" alt="" /></a>
+			<a href="#" target="_blank"><img src="img/1d.jpg" alt="" /></a>
+	    </div>
+	</div>
+	<div class="news">
+		<h2>值班安排</h2>
+		<hr >
+		<div class="schoolnews">
+			<ul >
+				<li><a href="">值班安排.....</a></li>
+				<li><a href="">值班安排........</a></li>
+				<li><a href="">值班安排...........</a></li>
+				<li><a href="">值班安排..............</a></li>
+				<li><a href="">值班安排.................</a></li>
+				<li><a href="">值班安排....................</a></li>
+				<li style="list-style:none;"><a href="">更多></a></li>
+		</ul>
+		</div>
+	</div> 
+
+	<div class="activity">
+	<div class="left">
+	<ul><h2 class="box">基地活动</h2>
+
+
+		<div>	
+			<div class="time">
+				<div class="month">oct</div>
+				<div class="day">20</div>
+			</div>
+		
+		
+			<div class="act">
+				<h3><a href="">周末聚餐</a></h3>
+				<p><img src="img/iconfont-time.png" alt="" width="14px" height="14px" />2015/10/19 14:30</p>
+				<p><img src="img/iconfont-position-o.png" alt="" width="14px" height="14px" />研究生117报告厅</p>
+			</div>
+		</div>
+		
+		<div>
+			<div class="time">
+				<div class="month">oct</div>
+				<div class="day">20</div>
+			</div>
+			<div class="actinfo">
+				<h3><a href="">马云捐献100亿以支持南昌大学的建设</a></h3>
+				<p><img src="img/iconfont-time.png" alt="" width="14px" height="14px" />2015/10/20 10:00</p>
+				<p><img src="img/iconfont-position-o.png" alt="" width="14px" height="14px" />信工楼A104</p>
+			</div>
+		</div>
+		<a href=""style="text-decoration:none">更多></a>
+		
+		
+	</ul>
+	</div>
+	<div class="notice">
+		<h2 class="box">基地公告</h2>
+		
+		<ul class="middle">
+			 <li><a href="">物联网实验基地2016年青年千人、长江学者招聘</a></li>
+			<li><a href="">物联网实验基地2016年人才招聘广告</a></li>
+			<li><a href="">关于做好2016年物联网实验基地青年马克思主义者...</a></li>
+			<li><a href="">关于切实做好2016年国庆期间物联网实验基地安全稳...</a></li>
+			<li class="special"><a href="">更多></a></li> 
+			
+		</ul>
+	</div>
+	
+	<div class="right">
+		<ul class="zhuanti">
+			<li class="ann" ><a href=""><strong>......活动专区.........</strong></a></li>
+			<li class="ann"><a href=""><strong>......小组展示........</strong></a></li>
+			<li class="ann"><a href=""><strong>......快乐生活.........</strong></a></li>
+			<li class="special">更多></li>
+		</ul>
+	</div>
+	
+	</div>
+
+	<div class="message">
+	<div class="zuo">
+	
+		<ul>
+			<li class="part1"> <img class="tubiao"src="img/iconfont-shuben.png" alt="" /> <a href=""title="Click to view member information">GROUP-1</a></li>
+			<li class="part2"> <img class="tubiao"src="img/iconfont-iphone.png" alt="" /> <a href=""title="Click to view member information">GROUP-2</a></li>
+			<li class="part3"> <img class="tubiao"src="img/iconfont-weixin.png" alt="" /> <a href=""title="Click to view member information"> GROUP-3</a></li>
+		</ul>
+	</div>
+	 <div class="zhong">
+		<ul>
+			<li class="part4"><img class="tubiao"src="img/iconfont-gerenxinxi.png" alt="" /><a href=""title="Click to view member information">GROUP-4</a></li>
+			<li class="part5"><img class="tubiao"src="img/iconfont-yingxiangsheying.png" alt="" /><a href=""title="Click to view member information">GROUP-5</a></li>
+			<li class="part6"><img class="tubiao"src="img/iconfont-address.png" alt="" /><a href=""title="Click to view member information">GROUP-6</a></li>
+		</ul>
+	
+	</div>
+	
+	<div class="you">
+		<ul>
+			<li class="part7"><img class="tubiao"src="img/iconfont-dianhua.png" alt="" /><a href=""title="Click to view member information">GROUP-7</a></li>
+			<li class="part8"><img class="tubiao"src="img/iconfont-mailline.png" alt="" /><a href=""title="Click to view member information">GROUP-8</a><li>
+			<li class="part9"><img class="tubiao"src="img/iconfont-address.png" alt="" /><a href="group9.php"title="Click to view member information">GROUP-9</a></li>
+		</ul>
+	
+	</div>
+	
+	<div class="lianjie">
+	
+	<h2>快速通道</h2>
+	<hr />
+		<div class="ali"><a href="">教务在线</a>
+		<a href="">团学时空</a>
+		<a href="">家园网</a>
+		<a href="">研究生网</a>
+		<a href="">心理之家</a>
+		</div> 
+	<h2>友情链接</h2>
+	<hr />
+	<div class="ali"><a href="IOT.html">物联网基地软件组</a>
+		<a href="">科学技术部</a>
+		<a href="">江西省教育厅</a>
+		<br />
+		<a href="">江西省科学技术厅</a>
+		<a href="">中西部大学联盟网</a>
+		</div> 
+
+
+
+	</div>
+	
+	</div>
+
+	<div class="bottom">
+		<div class="bottom-1"><ul>
+				<li><a href="">地理位置</a></li>
+				<li><a href="">网站地图</a></li>
+				<li><a href="">创作团队</a></li>
+				<li><a href="">意见反馈</a></li>
+			</ul></div>
+		<div class="bottom-2"><p>Copyright (c) 2016 Nanchang University. All Rights Reserved. 赣B2-20050168号/网备36011002000011号</p></div>
+	</div>
+	</body>
+</html>
